2a8c49c3fc591d7200ab4f449a3585a712737fab
OtmaneAzeroualDZHW <56314235+OtmaneAzeroualDZHW@users.noreply.github.com>
2021-02-01 19:44:46 +0100

    Add files via upload

---
+++ Code 3 Reducer for WordCount in Hadoop.java
@@ -0,0 +1,16 @@
+public class Reduce
+implements Reducer<Text, IntWritable,
+Text, IntWritable> {
+	
+@Override
+public void reduce(Text key, Iterator<IntWritable> values,
+OutputCollector<Text, IntWritable> output,
+Reporter reporter) throws IOException {
+	int sum = 0;
+	while (values.hasNext()) {
+	 sum += values.next().get();
+}
+    output.collect(key, new IntWritable(sum));
+	
+}
+}
